e3cfc512bd26f76b83a185008218bc1e0b52b1b3
Vanshika Saxena <vanshika_saxena@persistent.com>
2024-12-17 13:44:42 +0530

    initial user page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"library-management-system",component:HomePageComponent},
+  {path:'**',redirectTo:'/library-management-system',pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
